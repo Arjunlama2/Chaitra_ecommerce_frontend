3f65946cd732284bee102a916eec3ba109d90cd5
Arjunlama2 <instructorarjun1@gmail.com>
2025-06-22 16:58:40 +0545

    navbar and hero

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Header from '../../components/landing/Header'
+import Navbar from '../../components/landing/Navbar'
+import Hero from '../../components/landing/Hero'
 
 function Home() {
   return (
     <>
     <Header/>
+    <Navbar/>
+    <Hero/>
     </>
   )
 }
